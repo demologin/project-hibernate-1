c992af557ba7b9a82c3759b358c759f8467463df
RomanKotovych <kotovich.roman2010@gmail.com>
2024-05-15 20:18:49 -0500

    Revert "added p6spy"
    This reverts commit f60d8e2c5066c4d7cfa6eab03848fb41220f49a5.

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -23,8 +23,8 @@ public class PlayerRepositoryDB implements IPlayerRepository {
         Properties properties = new Properties();
 
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
         properties.put(Environment.SHOW_SQL, "true");
         properties.put(Environment.FORMAT_SQL, "true");
         properties.put(Environment.USER, "root");
